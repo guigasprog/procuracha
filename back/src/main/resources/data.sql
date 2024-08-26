49f09fd727a148ddd6b4746f3e480e1391e37658
Guilherme Delgado Martins <2311600261@fema.edu.br>
2024-08-26 11:56:39 -0300

    feat: inclementar sql insert para h2 database

---
+++ back/src/main/resources/data.sql
@@ -0,0 +1,6 @@
+INSERT INTO cidade (id, nome, uf) VALUES (1, 'São Paulo', 'SP');
+INSERT INTO cidade (id, nome, uf) VALUES (2, 'Campinas', 'SP');
+INSERT INTO cidade (id, nome, uf) VALUES (3, 'Rio de Janeiro', 'RJ');
+INSERT INTO cidade (id, nome, uf) VALUES (4, 'Niterói', 'RJ');
+INSERT INTO cidade (id, nome, uf) VALUES (5, 'Belo Horizonte', 'MG');
+INSERT INTO cidade (id, nome, uf) VALUES (6, 'Uberlândia', 'MG');
