d162e7ab8e3f2df6dd59a5541c45d480f6fb2f39
Guilherme Delgado Martins <2311600261@fema.edu.br>
2024-08-26 12:34:29 -0300

    feat: adicionar mais inserts agora da tabela servico

--- back/src/main/resources/data.sql
+++ back/src/main/resources/data.sql
@@ -4,3 +4,9 @@ INSERT INTO cidade (id, nome, uf) VALUES (3, 'Rio de Janeiro', 'RJ');
 INSERT INTO cidade (id, nome, uf) VALUES (4, 'Niterói', 'RJ');
 INSERT INTO cidade (id, nome, uf) VALUES (5, 'Belo Horizonte', 'MG');
 INSERT INTO cidade (id, nome, uf) VALUES (6, 'Uberlândia', 'MG');
+
+INSERT INTO servico (id, descricao, valor) VALUES (1, 'Corte de Cabelo', 50.00);
+INSERT INTO servico (id, descricao, valor) VALUES (2, 'Manicure', 30.00);
+INSERT INTO servico (id, descricao, valor) VALUES (3, 'Massagem Relaxante', 150.00);
+INSERT INTO servico (id, descricao, valor) VALUES (4, 'Limpeza de Pele', 120.50);
+INSERT INTO servico (id, descricao, valor) VALUES (5, 'Depilação', 80.75);
