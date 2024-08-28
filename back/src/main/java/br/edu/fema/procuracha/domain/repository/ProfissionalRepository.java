1ae0544c9c224549945f976911e8c93832a8a0d7
Guilherme Delgado Martins <2311600261@fema.edu.br>
2024-08-28 12:41:36 -0300

    feat: inclementar ProfissionalRepository

--- back/src/main/java/br/edu/fema/procuracha/domain/repository/ProfissionalRepository.java
+++ back/src/main/java/br/edu/fema/procuracha/domain/repository/ProfissionalRepository.java
@@ -3,5 +3,11 @@ package br.edu.fema.procuracha.domain.repository;
 import br.edu.fema.procuracha.domain.entity.ProfissionalEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ProfissionalRepository extends JpaRepository<ProfissionalEntity, Long> {
+
+    ProfissionalEntity findByClienteEntity_Id(Long id);
+
+    List<ProfissionalEntity> findAllByIdNot(Long id);
 }
