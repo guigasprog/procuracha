bedc8060c03c85d08b624b64cc22f0fcb16392bd
Guilherme Delgado Martins <2311600261@fema.edu.br>
2024-08-28 12:40:57 -0300

    fix: arrumar ProfissionalDTO

--- back/src/main/java/br/edu/fema/procuracha/application/dto/ProfissionalDTO.java
+++ back/src/main/java/br/edu/fema/procuracha/application/dto/ProfissionalDTO.java
@@ -1,14 +1,25 @@
 package br.edu.fema.procuracha.application.dto;
 
+import br.edu.fema.procuracha.domain.entity.FeedbackEntity;
 import br.edu.fema.procuracha.domain.entity.ProfissionalEntity;
+import br.edu.fema.procuracha.domain.repository.FeedbackRepository;
 import lombok.Getter;
+import lombok.Setter;
+import org.springframework.beans.factory.annotation.Autowired;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Getter
+@Setter
 public class ProfissionalDTO {
 
     private Long id;
+
+    private Long mediaFeedback;
+
+    private List<FeedbackProfissionalDTO> feedbacksProfissional;
 
     private ClienteDTO cliente;
 
@@ -20,8 +31,30 @@ public class ProfissionalDTO {
         this.servicos = ServicoDTO.converterEntityForDTO(profissionalEntity.getServicoEntities());
     }
 
-    public static List<ProfissionalDTO> converterEntityForDTO(List<ProfissionalEntity> profissionalEntityList) {
-        return profissionalEntityList.stream().map(ProfissionalDTO::new).toList();
+    public ProfissionalDTO(ProfissionalEntity profissionalEntity, List<FeedbackEntity> feedbackEntity) {
+        this.id = profissionalEntity.getId();
+        this.cliente = new ClienteDTO(profissionalEntity.getClienteEntity());
+        this.servicos = ServicoDTO.converterEntityForDTO(profissionalEntity.getServicoEntities());
+        this.mediaFeedback = 0L;
+        if (feedbackEntity != null) {
+            this.feedbacksProfissional = FeedbackProfissionalDTO.converterEntityForDto(feedbackEntity);
+            this.feedbacksProfissional.stream()
+                    .forEach(profissional -> this.mediaFeedback += profissional.getNota());
+            this.mediaFeedback /= feedbacksProfissional.size();
+        } else {
+            this.feedbacksProfissional = new ArrayList<>(); // ou null, dependendo da sua necessidade
+        }
+    }
+
+    public static List<ProfissionalDTO> converterEntityForDTO(List<ProfissionalEntity> profissionalEntity) {
+        return profissionalEntity.stream().map(ProfissionalDTO::new).toList();
+    }
+
+    public static List<ProfissionalDTO> converter(List<ProfissionalEntity> profissionalEntities, FeedbackRepository feedbackRepository) {
+        return profissionalEntities.stream()
+                .map(profissionalEntity -> new ProfissionalDTO(profissionalEntity,
+                        feedbackRepository.findAllByContratoEntity_ProfissionalEntity_Id(profissionalEntity.getId())))
+                .collect(Collectors.toList());
     }
 
 }
