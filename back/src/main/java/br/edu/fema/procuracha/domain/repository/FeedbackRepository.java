7752e928b50e746dbd23aac46b372a0e41b11cfc
Guilherme Delgado Martins <2311600261@fema.edu.br>
2024-08-28 12:40:46 -0300

    feat: inclementar FindBys FeedbackRepository

--- back/src/main/java/br/edu/fema/procuracha/domain/repository/FeedbackRepository.java
+++ back/src/main/java/br/edu/fema/procuracha/domain/repository/FeedbackRepository.java
@@ -3,5 +3,10 @@ package br.edu.fema.procuracha.domain.repository;
 import br.edu.fema.procuracha.domain.entity.FeedbackEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface FeedbackRepository extends JpaRepository<FeedbackEntity, Long> {
+
+    List<FeedbackEntity> findAllByContratoEntity_ProfissionalEntity_Id(Long id);
+
 }
